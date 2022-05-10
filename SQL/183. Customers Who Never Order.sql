65748daa3780bf965f757c15fe7b944a590a8b51
azratuni <18201060@uap-bd.edu>
2022-05-10 21:31:34 +0600

    SQL

---
+++ SQL/183. Customers Who Never Order.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT name AS Customers FROM Customers
+WHERE NOT EXISTS 
+(   SELECT customerId FROM Orders
+    WHERE Customers.id = Orders.customerId)
+
+
